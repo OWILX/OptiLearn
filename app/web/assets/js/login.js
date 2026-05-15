02a0dd7cc795d1e7d0780a96a05b768e1f68ed22
OWILX <greatenyinna10@gmail.com>
2026-05-15 15:49:45 +0100

    mes

--- app/web/assets/js/login.js
+++ app/web/assets/js/login.js
@@ -51,3 +51,30 @@ const { data } = await client.auth.getSession();
 if(data.session){
    window.location.href = "home.html";
 }
+const supabaseUrl =  'https://jxdllmwdyvhcsdvodnbb.supabase.co/rest/v1/';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZGxsbXdkeXZoY3Nkdm9kbmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMDg2OTgsImV4cCI6MjA5Mjg4NDY5OH0.KlkSqiB_KRT6aQdN-olzIo6sYLZQ0ECA9KBoUs6F44g';
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+document.getElementById("signup-btn").addEventListener("click", async (e) => {
+    e.preventDefault();
+        //Sign-up
+    const name = document.getElementById("signup-name").value;
+    const email = document.getElementById("signup-email").value;
+    const password = document.getElementById("signup-password").value;
+
+    const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        options: {
+            data: {
+                full_name: name
+            }
+        }
+    });
+
+    if(error){
+        alert(error.message);
+    } else {
+        alert("Account created! Check your email.");
+    }
+})
